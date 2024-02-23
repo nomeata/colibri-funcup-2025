927369ae6a99a78e6a3c7e24ed1eab90e1e91a4b
Joachim Breitner <mail@joachim-breitner.de>
2024-02-23 19:52:46 +0100

    Switch to 2024

--- fetch-flights.sh
+++ fetch-flights.sh
@@ -3,13 +3,13 @@
 set -e
 
 limit=2000
-year=2023
+year=2024
 
 echo "flights.json: fetching"
 wget \
     --no-verbose \
 	--load-cookies _tmp/cookies.txt \
-    "https://de.dhv-xc.de/api/fli/flights?d0=1.1.$year&d1=31.11.$year&fkto%5B%5D=9306&fkto%5B%5D=11362&clubde%5B%5D=130&navpars=%7B%22start%22%3A0%2C%22limit%22%3A$limit%7D" \
+    "https://de.dhv-xc.de/api/fli/flights?d0=1.1.$year&d1=15.9.$year&fkto%5B%5D=9306&fkto%5B%5D=11362&clubde%5B%5D=130&navpars=%7B%22start%22%3A0%2C%22limit%22%3A$limit%7D" \
 	-O _tmp/flights.json.tmp
 echo -n "Flights before opt-out: "
 jq '.data | length' < _tmp/flights.json.tmp
